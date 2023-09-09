82c598561bce543f560d18ae39f7037929f09732
srzh <61797666+88srzh@users.noreply.github.com>
2023-09-09 22:43:14 +0400

    step to add hotel response

---
+++ lib/domain/api_client/hotel_api_client.dart
@@ -0,0 +1,13 @@
+import 'package:hotel_app/domain/api_client/network_client.dart';
+
+class HotelApiClient {
+  final _networkClient = NetworkClient();
+
+  // Future<HotelResponse> hotel(
+  //     String locale;
+  //     String apiKey;
+  //     ) async {
+  //
+  // }
+
+}
---
+++ lib/domain/blocs/hotel/hotel_bloc.dart
@@ -0,0 +1,11 @@
+
+
+// part 'hotel_bloc.freezed.dart';
+
+// part 'hotel_event.dart';
+
+// part 'hotel_state';
+
+// class HotelBloc extends Bloc<HotelEvent, HotelState> {
+  // final _apiClient = ApiCl
+// }
