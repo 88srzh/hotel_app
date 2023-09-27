3fb0539f6b17cd25415bcc1969a8526b67f19232
hu <19srzh88@gmail.com>
2023-09-27 11:17:38 +0400

    added network request

--- lib/domain/api_client/new_network_client.dart
+++ lib/domain/api_client/new_network_client.dart
@@ -1,5 +1,5 @@
 import 'package:http/http.dart' as http;
 
-Future<http.Response> fetchPhotos(http.Client client) async {
-  return client.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+Future<http.Response> fetchData(http.Client client) async {
+  return client.get(Uri.parse('https://https://run.mocky.io/v3/35e0d18e-2521-4f1b-a575-f0fe366f66e3'));
 }
