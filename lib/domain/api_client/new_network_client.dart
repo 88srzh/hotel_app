54367ebbaaa65abde91eb7c21f5254bd068f90bb
hu <19srzh88@gmail.com>
2023-09-27 11:31:38 +0400

    convert the response into a list of hotel

--- lib/domain/api_client/new_network_client.dart
+++ lib/domain/api_client/new_network_client.dart
@@ -1,5 +1,14 @@
+import 'dart:convert';
+
+import 'package:hotel_app/domain/entity/hotel.dart';
 import 'package:http/http.dart' as http;
 
-Future<http.Response> fetchData(http.Client client) async {
-  return client.get(Uri.parse('https://https://run.mocky.io/v3/35e0d18e-2521-4f1b-a575-f0fe366f66e3'));
+List<Hotel> parseHotel(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<Hotel>((json) => Hotel.fromJson(json)).toList();
+}
+
+Future<List<Hotel>> fetchData(http.Client client) async {
+  final response = await client.get(Uri.parse('https://https://run.mocky.io/v3/35e0d18e-2521-4f1b-a575-f0fe366f66e3'));
+  return parseHotel(response.body);
 }
