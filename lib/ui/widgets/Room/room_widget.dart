0daf0f54a55aebf6a74dd50ae911997251ea2f1f
hu <19srzh88@gmail.com>
2023-10-02 11:31:14 +0400

    added room entity

---
+++ lib/ui/widgets/Room/room_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RoomWidget extends StatelessWidget {
+  const RoomWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Steigenberger Makadi')),
+      body: ListView(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
