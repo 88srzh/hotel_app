0f9d6d330a789db7413e1ef8e9519a8dfc852413
hu <19srzh88@gmail.com>
2023-10-03 13:43:04 +0400

    added network data for reservation

--- lib/domain/api_client/network_client.dart
+++ lib/domain/api_client/network_client.dart
@@ -16,7 +16,8 @@ class Network {
       return 'Not found';
     } else if (response.statusCode == 500) {
       return 'Server not responding';
-    }  return 'Network error';
+    }
+    return 'Network error';
   }
 }
 
@@ -28,6 +29,12 @@ Future<String> getNetworkData() async {
 
 Future<String> getNetworkDataForRoom() async {
   final network = Network('https://run.mocky.io/v3/f9a38183-6f95-43aa-853a-9c83cbb05ecd');
+  final networkData = await network.getData();
+  return networkData;
+}
+
+Future<String> getNetworkDataFromReservation() async {
+  final network = Network('https://run.mocky.io/v3/e8868481-743f-4eb2-a0d7-2bc4012275c8');
   final networkData = await network.getData();
   return networkData;
 }
